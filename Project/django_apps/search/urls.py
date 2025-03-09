33d5e226b5b8296468d8cce5716ff2d5c043a397
Hao <133094513+Ha0c4@users.noreply.github.com>
2025-03-09 17:54:25 +1100

    Update

--- Project/django_apps/search/urls.py
+++ Project/django_apps/search/urls.py
@@ -1,7 +1,7 @@
 # print("***** LOADED search/urls.py *****")
 
 # from django.urls import path
-# from . import views
+from . import views
 
 # urlpatterns = [
 #     path('', views.search, name='search'),
@@ -12,5 +12,6 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-]
+     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+     path('index_content/', views.index_content, name='index_content'),
+ ]
